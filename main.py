b3e67f9cc77864673aeb3481451e80fb360d0abb
brandonlbaez <104741794+brandonlbaez@users.noreply.github.com>
2022-05-01 20:40:16 -0400

    Add files via upload
    working version. randomly chooses between 3 words and gives you 5 tries to guess the word. prints "you win" if guessed within the 5 attempts and "you lose" if you don't guess the word.

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 import random
-import copy
 attempts = 5
 wordlist = ["banana", "apple", "orange"]
 chosen_word = list(random.choice(wordlist))
-reference_word = copy.deepcopy(chosen_word)
-print(reference_word)
-for i in range(len(chosen_word)):
-    chosen_word[i] = '*'
-    hidden_word = chosen_word[i]
+hidden_word = ["*"] * len(chosen_word)
 print("this is hangman. guess a letter and hit enter.")
 while attempts > 0:
     guess = input()
-    if guess == reference_word[0:]:
-        for i in range(len(reference_word)):
-            if reference_word[i] == guess:
-                hidden_word = list(map(lambda item: item.replace("*",input()), hidden_word))
-                print(hidden_word)
+    if guess in chosen_word:
+        for i in range(len(hidden_word)):
+            if chosen_word[i] == guess:
+                hidden_word[i] = guess
+        print(hidden_word)
     else:
         attempts = attempts - 1
         print(False)
         print(attempts)
-    if guess == chosen_word:
+    if hidden_word == chosen_word:
         print("you win.")
         break
 if attempts == 0:
