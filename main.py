1dd4867a262a41ba4fb389b6a06d2c7a95a8f71f
brandonlbaez <104741794+brandonlbaez@users.noreply.github.com>
2022-05-01 20:45:40 -0400

    Add files via upload
    added future goals to code. reveals word when you lose.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
 attempts = 5
-wordlist = ["banana", "apple", "orange"]
+wordlist = ["banana", "apple", "orange"] # add larger library of words.
 chosen_word = list(random.choice(wordlist))
 hidden_word = ["*"] * len(chosen_word)
 print("this is hangman. guess a letter and hit enter.")
 while attempts > 0:
-    guess = input()
+    guess = input() # only input one letter at a time. no numbers or special characters. prevent user from guessing same letter.
     if guess in chosen_word:
         for i in range(len(hidden_word)):
             if chosen_word[i] == guess:
@@ -19,4 +19,5 @@ while attempts > 0:
         print("you win.")
         break
 if attempts == 0:
-    print("you lose.")
+    print("you lose. here was the word:")
+    print(chosen_word) # make it a string, not a list.
